47ab990e58abc0ce1f97171c546cca911e7c9047
Andre-210 <andre.melendez5835@gmail.com>
2024-02-14 11:17:00 -0700

    Updated the prompt method.

--- Lab_3/Lab_3_Chess_Pieces_with_Hierarchy_and_Polymorphism.java
+++ Lab_3/Lab_3_Chess_Pieces_with_Hierarchy_and_Polymorphism.java
@@ -23,6 +23,8 @@ public class Lab_3_Chess_Pieces_with_Hierarchy_and_Polymorphism {
         for (int count = 0; count < 6; count++) {
             // try catch block for errors
             try {
+                // initialize a Boolean and set it to false'
+                Boolean exists = false;
                 // print to prompt the user to input a chess piece with a position
                 System.out.println("Input a chess piece with its initial position. Can only input a chess piece once. E.G. Pawn, White, H, 3. Input 'stop' to end the program ... ");
                 System.out.println("");
@@ -39,6 +41,8 @@ public class Lab_3_Chess_Pieces_with_Hierarchy_and_Polymorphism {
                 for (int idx = 0; idx < count; idx++) {
                     // if the users inputted chess piece already exists in the pieces array
                     if (chess_piece_type.valueOf(user_info[0]) == pieces[idx].getType()) {
+                        // change the exists Boolean to true
+                        exists = true;
                         // let the user know
                         System.out.println("The inputted chess piece already exists, please input a non-existing chess piece ...");
                         System.out.println("");
@@ -48,27 +52,33 @@ public class Lab_3_Chess_Pieces_with_Hierarchy_and_Polymorphism {
                         break;
                     }
                 }
-                // check if the users inputted initial position is within range of the chessboard
-                if (!chessboard.withinChessboard(chess_piece_columns.valueOf(user_info[2]), Integer.parseInt(user_info[3]))) {
-                    // let the user know that the inputted position is not within range
-                    System.out.println("User input for starting position is out of range ... ");
-                    System.out.println("");
-                    // decrementcount since the chess piece was invalid
-                    count--;
-                    // loop back up to prompt the user again
-                    continue;
-                }
-                // create variables to hold the users inputs to create the object
-                chess_piece_type type = chess_piece_type.valueOf(user_info[0]);
-                chess_piece_color color = chess_piece_color.valueOf(user_info[1]);
-                chess_piece_columns x_coord = chess_piece_columns.valueOf(user_info[2]);
-                int y_coord = Integer.parseInt(user_info[3]);
-                // create the object using the users inputs and store in the pieces array
-                pieces[count] = chessPiece.create_chess_piece(type, color, x_coord, y_coord);
-                // let the user know that the chess piece was create
-                System.out.println("The " + type + " chess piece has been successfully created ...");
-                System.out.println("");
-            } 
+                // if the chess piece does not exist
+                if (!exists) {
+                    // check if the users inputted initial position is within range of the chessboard
+                    if (!chessboard.withinChessboard(chess_piece_columns.valueOf(user_info[2]), Integer.parseInt(user_info[3]))) {
+                        // let the user know that the inputted position is not within range
+                        System.out.println("User input for starting position is out of range ... ");
+                        System.out.println("");
+                        // decrementcount since the chess piece was invalid
+                        count--;
+                        // loop back up to prompt the user again
+                        continue;
+                    }
+                    // otherwise, the coordinate is valid
+                    else {
+                        // create variables to hold the users inputs to create the object
+                        chess_piece_type type = chess_piece_type.valueOf(user_info[0]);
+                        chess_piece_color color = chess_piece_color.valueOf(user_info[1]);
+                        chess_piece_columns x_coord = chess_piece_columns.valueOf(user_info[2]);
+                        int y_coord = Integer.parseInt(user_info[3]);
+                        // create the object using the users inputs and store in the pieces array
+                        pieces[count] = chessPiece.create_chess_piece(type, color, x_coord, y_coord);
+                        // let the user know that the chess piece was create
+                        System.out.println("The " + type + " chess piece has been successfully created ...");
+                        System.out.println("");
+                    }
+                } 
+            }
             catch (Exception e) {
                 System.out.println("Invalid input, try again ...");
                 System.out.println("");
@@ -86,7 +96,7 @@ public class Lab_3_Chess_Pieces_with_Hierarchy_and_Polymorphism {
         int i = 0;
         String[] user_input;
         String newMove;
-        System.out.println("Input a new position for the pieces to try to move to. ");
+        System.out.println("Input a new position for the pieces to try to move to. Input 'stop to end program .... ");
         newMove = scnr.nextLine();
         // if the usrs input is 'stop' 
          if (newMove.toUpperCase().equals("STOP")) {
